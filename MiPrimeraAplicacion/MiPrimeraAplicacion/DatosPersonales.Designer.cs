8c24b6091cc1d7f05c2094a41e0ed5aa2bfee765
Matias Delis Martinez <matiasdelis@hotmail.com>
2023-01-24 12:55:56 -0300

    Primera app

---
+++ MiPrimeraAplicacion/MiPrimeraAplicacion/DatosPersonales.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace MiPrimeraAplicacion
+{
+    partial class DatosPersonales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.lblcorreccion1 = new System.Windows.Forms.Label();
+            this.lblcorrecion2 = new System.Windows.Forms.Label();
+            this.lblcorreccion3 = new System.Windows.Forms.Label();
+            this.lblcorrecion4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(49, 42);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(71, 16);
+            this.lblApellido.TabIndex = 0;
+            this.lblApellido.Text = "APELLIDO";
+            
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(54, 79);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(66, 16);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "NOMBRE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "EDAD";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "DIRECCION";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "RESULTADO";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtApellido.Location = new System.Drawing.Point(141, 36);
+            this.txtApellido.MaxLength = 15;
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(227, 22);
+            this.txtApellido.TabIndex = 5;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNombre.Location = new System.Drawing.Point(141, 73);
+            this.txtNombre.MaxLength = 20;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(227, 22);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(141, 110);
+            this.txtEdad.MaxLength = 3;
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(63, 22);
+            this.txtEdad.TabIndex = 7;
+            this.txtEdad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEdad_KeyPress);
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtDireccion.Location = new System.Drawing.Point(141, 150);
+            this.txtDireccion.MaxLength = 1000;
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(227, 22);
+            this.txtDireccion.TabIndex = 8;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtResultado.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtResultado.Location = new System.Drawing.Point(43, 250);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResultado.Size = new System.Drawing.Size(325, 162);
+            this.txtResultado.TabIndex = 9;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(108, 418);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(78, 23);
+            this.btnAceptar.TabIndex = 10;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(222, 418);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(82, 23);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // lblcorreccion1
+            // 
+            this.lblcorreccion1.AutoSize = true;
+            this.lblcorreccion1.ForeColor = System.Drawing.Color.Red;
+            this.lblcorreccion1.Location = new System.Drawing.Point(126, 42);
+            this.lblcorreccion1.Name = "lblcorreccion1";
+            this.lblcorreccion1.Size = new System.Drawing.Size(12, 16);
+            this.lblcorreccion1.TabIndex = 12;
+            this.lblcorreccion1.Text = "*";
+            this.lblcorreccion1.Visible = false;
+            // 
+            // lblcorrecion2
+            // 
+            this.lblcorrecion2.AutoSize = true;
+            this.lblcorrecion2.ForeColor = System.Drawing.Color.Red;
+            this.lblcorrecion2.Location = new System.Drawing.Point(126, 79);
+            this.lblcorrecion2.Name = "lblcorrecion2";
+            this.lblcorrecion2.Size = new System.Drawing.Size(12, 16);
+            this.lblcorrecion2.TabIndex = 13;
+            this.lblcorrecion2.Text = "*";
+            this.lblcorrecion2.Visible = false;
+            // 
+            // lblcorreccion3
+            // 
+            this.lblcorreccion3.AutoSize = true;
+            this.lblcorreccion3.ForeColor = System.Drawing.Color.Red;
+            this.lblcorreccion3.Location = new System.Drawing.Point(126, 113);
+            this.lblcorreccion3.Name = "lblcorreccion3";
+            this.lblcorreccion3.Size = new System.Drawing.Size(12, 16);
+            this.lblcorreccion3.TabIndex = 14;
+            this.lblcorreccion3.Text = "*";
+            this.lblcorreccion3.Visible = false;
+            // 
+            // lblcorrecion4
+            // 
+            this.lblcorrecion4.AutoSize = true;
+            this.lblcorrecion4.ForeColor = System.Drawing.Color.Red;
+            this.lblcorrecion4.Location = new System.Drawing.Point(126, 150);
+            this.lblcorrecion4.Name = "lblcorrecion4";
+            this.lblcorrecion4.Size = new System.Drawing.Size(12, 16);
+            this.lblcorrecion4.TabIndex = 15;
+            this.lblcorrecion4.Text = "*";
+            this.lblcorrecion4.Visible = false;
+            // 
+            // DatosPersonales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(401, 450);
+            this.Controls.Add(this.lblcorrecion4);
+            this.Controls.Add(this.lblcorreccion3);
+            this.Controls.Add(this.lblcorrecion2);
+            this.Controls.Add(this.lblcorreccion1);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblApellido);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(419, 497);
+            this.MinimumSize = new System.Drawing.Size(419, 497);
+            this.Name = "DatosPersonales";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Datos Personales";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label lblcorreccion1;
+        private System.Windows.Forms.Label lblcorrecion2;
+        private System.Windows.Forms.Label lblcorreccion3;
+        private System.Windows.Forms.Label lblcorrecion4;
+    }
+}
---
+++ MiPrimeraAplicacion/MiPrimeraAplicacion/DatosPersonales.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimeraAplicacion
+{
+    public partial class DatosPersonales : Form
+    {
+        public DatosPersonales()
+        {
+            InitializeComponent();
+        }
+
+        private void txtEdad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ( (e.KeyChar < 48 || e.KeyChar > 59) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            bool bandera = false;
+
+            if(txtApellido.Text == "")
+            {
+                lblcorreccion1.Visible= true;
+                bandera= true;
+            }
+            else
+            {
+                lblcorreccion1.Visible = false;
+                txtApellido.BackColor = System.Drawing.SystemColors.Window;
+            }
+
+            if (txtNombre.Text == "")
+            {
+                lblcorrecion2.Visible= true;
+                bandera= true;
+            }
+            else
+            {
+                txtNombre.BackColor = System.Drawing.SystemColors.Window;
+                lblcorrecion2.Visible = false;
+            }
+            if (txtEdad.Text == "")
+            {
+                lblcorreccion3.Visible= true;
+                bandera = true;
+            }
+            else
+            {
+                txtEdad.BackColor = System.Drawing.SystemColors.Window;
+                lblcorreccion3.Visible = false;
+            }
+            if (txtDireccion.Text == "")
+            {
+                lblcorrecion4.Visible= true;
+                bandera = true;
+            }
+            else
+            {
+                txtDireccion.BackColor = System.Drawing.SystemColors.Window;
+                lblcorrecion4.Visible = false;
+            }
+
+            if (bandera)
+            {
+                MessageBox.Show("Complete todos los campos.");
+            }
+
+            string apellido = txtApellido.Text;
+            string nombre   = txtNombre.Text;
+            string edad = txtEdad.Text;
+            string direccion = txtDireccion.Text;
+
+            if (txtApellido.Text != "" && txtNombre.Text != "" && txtEdad.Text != "" && txtDireccion.Text != "")
+            {
+                txtResultado.Text = apellido + " " + nombre + Environment.NewLine + edad + Environment.NewLine + direccion;
+                
+            }
+            
+            
+        }
+    }
+}
